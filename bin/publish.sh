7927e1b6b9fe9cec6b7cd19fccb460c61d6e1284
FLSoz <65517812+FLSoz@users.noreply.github.com>
2022-08-06 12:30:14 -0700

    Automate publishing

---
+++ bin/publish.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ $# -gt 0 ]; then
+	export GH_TOKEN=$1
+
+	# Push version tag to remote
+
+	# do the build
+	ts-node ./.erb/scripts/clean.js dist
+	npm run build
+	PLATFORM=$(node --eval "process.stdout.write(require('os').platform())")
+
+	case $PLATFORM in
+		"win32")
+			echo "Building for Windows"
+			electron-builder build --win --publish onTagOrDraft
+			;;
+		"linux")
+			echo "Building for Linux"
+			electron-builder build --linux --publish onTagOrDraft
+			;;
+		"darwin")
+			echo "Building for Mac"
+			electron-builder build --mac --publish onTagOrDraft
+			;;
+		*)
+			echo "UNKNOWN PLATFORM - CANNOT PUBLISH"
+			;;
+	esac
+else
+	echo "MUST SUPPLY GH TOKEN"
+fi
--- bin/version.sh
+++ bin/version.sh
@@ -8,11 +8,6 @@ if [ $# -ge 1 ]; then
 	git add .
 	git commit -m "Bump version to $version"
 	git tag $version HEAD
-	if [[ $# -ge 1 && $2 -gt 0 ]]; then
-		git push
-		# we push the tag
-		git push origin $version
-	fi
 else
 	echo "MUST SUPPLY A VERSION INCREMENT TYPE"
 fi
