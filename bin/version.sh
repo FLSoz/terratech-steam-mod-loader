0436e74f4400d75367b8396d0734246aee8973c0
FLSoz <65517812+FLSoz@users.noreply.github.com>
2022-08-06 11:56:25 -0700

    Edit publishing script

--- bin/version.sh
+++ bin/version.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-IncrType=$1
-version=$(npm version $IncrType --no-git-tag-version)
-cd release/app
-npm version $IncrType --no-git-tag-version
-cd ../../
-git add .
-git commit -m "Bump version to $version"
-git tag $version HEAD
+if [ $# -ge 1 ]; then
+	IncrType=$1
+	version=$(npm version $IncrType --no-git-tag-version)
+	cd release/app
+	npm version $IncrType --no-git-tag-version
+	cd ../../
+	git add .
+	git commit -m "Bump version to $version"
+	git tag $version HEAD
+	if [ $# -ge 1 && $2 -gt 0 ]; then
+		# we push the tag
+		git push origin $version
+	fi
+else
+	echo "MUST SUPPLY A VERSION INCREMENT TYPE"
+fi
