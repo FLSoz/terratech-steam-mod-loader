0de9abc7d469dd36a4e30b0694e4888f8a8a4443
FLSoz <65517812+FLSoz@users.noreply.github.com>
2022-08-21 19:42:36 -0700

    Fully automate release creation

--- publish-windows.ps1
+++ publish-windows.ps1
@@ -1,4 +1,14 @@
 git fetch;
 if ($?) { git push };
-if ($?) { git push origin v$(node --eval "process.stdout.write(require('./package.json').version)") };
+if ($?) {
+	$VERSION=$(node --eval "process.stdout.write(require('./package.json').version)")
+	$API_JSON="{`"tag_name`":`"v$VERSION`",`"target_commitish`":`"main`",`"name`":`"v$VERSION`",`"body`":`"Release of version $VERSION`",`"draft`":true,`"prerelease`":false}"
+	# ,`"discussion_category_name`":`"Announcements`"
+	$Headers=@{
+    'Accept' = 'application/vnd.github+json';
+		'Authorization' = "token $Env:GH_TOKEN"
+	}
+	git push origin v$VERSION
+	Invoke-WebRequest -UseBasicParsing -Body "$API_JSON" -Method 'POST' -Headers $Headers -Uri "https://api.github.com/repos/FLSoz/terratech-steam-mod-loader/releases"
+};
 if ($?) { npm run publish };
