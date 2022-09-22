d46474a284545d518ae40c5b7db1bcebff38010e
FLSoz <65517812+FLSoz@users.noreply.github.com>
2022-09-21 22:41:54 -0700

    update spacing to be consistent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,36 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-  extends: 'erb',
-  rules: {
-    // A temporary hack related to IDE not resolving correct package.json
-    'import/no-extraneous-dependencies': 'off',
-    'prettier/prettier': ['error', {
-			singleQuote: true,
-			trailingComma: 'none',
-			tabWidth: 2,
-			useTabs: true,
-			printWidth: 140,
-			bracketSameLine: false
-		}],
-		"no-restricted-imports": [
-      "error",
-      {
-        "patterns": ["@mui/*/*/*", "!@mui/material/test-utils/*"]
-      }
-    ],
-    'comma-dangle': ['error', 'never'],
-    'max-len': [
-      'warn',
-      {
-        code: 180,
-        ignoreComments: true,
+	extends: 'erb',
+	rules: {
+		// A temporary hack related to IDE not resolving correct package.json
+		'import/no-extraneous-dependencies': 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				trailingComma: 'none',
+				tabWidth: 2,
+				useTabs: true,
+				printWidth: 140,
+				bracketSameLine: false
+			}
+		],
+		'no-restricted-imports': [
+			'error',
+			{
+				patterns: ['@mui/*/*/*', '!@mui/material/test-utils/*']
+			}
+		],
+		'comma-dangle': ['error', 'never'],
+		'max-len': [
+			'warn',
+			{
+				code: 180,
+				ignoreComments: true,
 				ignoreUrls: true,
-        ignoreTrailingComments: true
-      }
-    ],
+				ignoreTrailingComments: true
+			}
+		],
 		'react/no-unused-state': 'warn',
 		'react/destructuring-assignment': 'warn',
 		'react/no-access-state-in-setstate': 'warn',
@@ -36,29 +39,29 @@ module.exports = {
 		'promise/always-return': 'warn',
 		'promise/catch-or-return': ['warn', { terminationMethod: ['catch', 'asCallback', 'finally'] }],
 		'@typescript-eslint/no-explicit-any': 'warn',
-		'no-console': "off",
+		'no-console': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
 		'class-methods-use-this': 'warn',
 		'prefer-destructuring': 'warn',
-    'react/require-default-props': 'off'
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-    createDefaultProgram: true
-  },
-  settings: {
-    'import/resolver': {
-      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
-      node: {},
-      webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.ts')
-      }
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx']
-    }
-  }
+		'react/require-default-props': 'off'
+	},
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		project: './tsconfig.json',
+		tsconfigRootDir: __dirname,
+		createDefaultProgram: true
+	},
+	settings: {
+		'import/resolver': {
+			// See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
+			node: {},
+			webpack: {
+				config: require.resolve('./.erb/configs/webpack.config.eslint.ts')
+			}
+		},
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx']
+		}
+	}
 };
